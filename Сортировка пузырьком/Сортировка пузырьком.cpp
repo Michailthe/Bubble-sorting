e5e0cc14f246e8b623dbf384315904e8398a59f6
Michailthe <112062292+Michailthe@users.noreply.github.com>
2024-10-18 19:51:01 +0300

    Добавьте файлы проекта.

---
+++ Сортировка пузырьком/Сортировка пузырьком.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Введите количество элементов массива: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Введите элементы массива: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Сортировка пузырьком
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                // Обмен элементов
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    // Вывод отсортированного массива
+    cout << "Отсортированный массив (по убыванию): ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
